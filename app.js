39cdf9cbc3a42caa566d5340a076add8156559cf
Milos Srejic <srejicmilos@hotmail.com>
2021-02-12 16:21:22 +0100

    dodao sam feature sat

--- app.js
+++ app.js
@@ -6,3 +6,18 @@ function promenaBoje() {
   console.log(r, g, b);
   document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
 }
+
+function sat() {
+  let date = new Date();
+  let sati = date.getHours();
+  let minuti = date.getMinutes();
+  let sekunde = date.getSeconds();
+
+  document.getElementById(
+    'sat'
+  ).innerHTML = `${sati}h : ${minuti}m : ${sekunde}s`;
+}
+
+setInterval(() => {
+  sat();
+}, 1000);
